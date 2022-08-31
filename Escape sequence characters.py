745d2236990456e641277ccf1ced8980f0be7f2d
Harshit Mandal <112474255+PythonProgrammerz@users.noreply.github.com>
2022-08-31 20:57:48 +0530

    Add files via upload

---
+++ Escape sequence characters.py
@@ -0,0 +1,12 @@
+"""
+Escape sequence characters
+are used in strings
+for a special meaning
+"""
+print("today is \nWednesday")
+print("today is \tWednesday")
+print("today is \rWednesday")
+print("today is \bWednesday")
+print("today is \"Wednesday")
+print("today is \'Wednesday")
+print("these are some basic escaoe sequence character")
---
+++ Taking input from user.py
@@ -0,0 +1,5 @@
+print("What is your name")
+name  = input()
+print("Where are you from")
+place = input()
+print("Hi",name+" from",place+" have a good day.")
