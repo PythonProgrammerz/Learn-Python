8ce35f15d4de6208693452c59d20b7e06cb70a84
Harshit Mandal <112474255+PythonProgrammerz@users.noreply.github.com>
2022-09-01 19:51:22 +0530

    Delete Learn-Python/Fun with numbers directory

--- Learn-Python/Fun with numbers/Adding currency.py
+++
@@ -1,7 +0,0 @@
-#Taking input and converting into float
-x = float(input("Enter a number: "))
-y = float(input("Enter another number: "))
-
-#Estimating and printing the result
-z = (x + y)
-print(f"{z:,}")
