7ce3ef73bc17d8fda927fb67b3032cc9a6db8e72
Harshit Mandal <112474255+PythonProgrammerz@users.noreply.github.com>
2022-09-03 19:33:42 +0530

    Update Taking input from user.py

--- Taking input from user.py
+++ Taking input from user.py
@@ -1,3 +1,3 @@
-name  = input(print("What is your name")).strip().Title()
-place = input(print("Where are you from")).strip().Title()
+name  = input(print("What is your name")).strip().title()
+place = input(print("Where are you from")).strip().title()
 print("Hi",name+" from",place+" have a good day.")
